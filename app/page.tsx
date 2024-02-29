b4cbb9dc60ccce28086857f0da7dbfceb4d40148
mohitarora249 <mohitarora249@gmail.com>
2024-02-29 08:17:53 +0000

    added kinde login package

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
 const Home = () => {
   return (
     <main className="">
-      Home
+      <LoginLink postLoginRedirectURL="/dashboard"><Button>Sign in</Button></LoginLink>
+      <RegisterLink postLoginRedirectURL="/welcome"><Button>Sign up</Button></RegisterLink>
     </main>
   )
 }
